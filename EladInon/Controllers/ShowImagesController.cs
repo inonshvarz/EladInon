c176047b39d726e943522b67ff2329b7f0755dcd
eladyek <eladyek@gmail.com>
2018-11-28 19:32:16 +0200

    add controller and view for pictures

---
+++ EladInon/Controllers/ShowImagesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EladInon.Controllers
+{
+    public class ShowImagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Search()
+        {
+            return null;
+        }
+    }
+}
