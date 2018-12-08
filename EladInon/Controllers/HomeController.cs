7f8b6d46a3e47094d0771551d5c13189a07cb760
inonshvarz <inonshvarz@gmail.com>
2018-12-08 22:22:55 +0200

    סתם שום כלום

--- EladInon/Controllers/HomeController.cs
+++ EladInon/Controllers/HomeController.cs
@@ -44,14 +44,4 @@ namespace EladInon.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
-    public class ImageModel : List<Image>
-    {
-        public ImageModel()
-        {
-         //   string directoryOfImage = HttpContext.Current.Server.MapPath("~/Images/");
-         //   XDocument imageData = XDocument.Load(directoryOfImage + @"/ImageMetaData.xml");
-         //   var images = from image in imageData.Descendants("image") select new Image(image.Element("filename").Value, image.Element("description").Value);
-         //   this.AddRange(images.ToList<Image>());
-        }
-    }
 }
--- EladInon/Controllers/ShowImagesController.cs
+++ EladInon/Controllers/ShowImagesController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using EladInon.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
